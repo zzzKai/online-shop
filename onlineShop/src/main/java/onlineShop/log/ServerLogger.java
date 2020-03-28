22fad59ff8105bfde51c4405ca47b4c8976b1a76
kai <zk.zhengkai@outlook.com>
2020-03-28 13:18:48 -0400

    update ServerLogger

--- onlineShop/src/main/java/onlineShop/log/ServerLogger.java
+++ onlineShop/src/main/java/onlineShop/log/ServerLogger.java
@@ -2,7 +2,8 @@ package onlineShop.log;
 
 import org.springframework.stereotype.Component;
 
-@Component(value = "serverLogger")
+// tell Spring this is a bean and Bean id
+@Component(value = "serverLogger") // Bean id
 public class ServerLogger implements Logger {
     public void log(String info) {
         System.out.println("server log = " + info);
